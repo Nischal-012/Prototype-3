af19b9b55cc4ebe4171af74031b077d3d1d56ab7
Nischal Joshi <076bct051@ioepc.edu.np>
2022-05-14 13:46:39 +0545

    Jump With Force Added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+    
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+      if(Input.GetKeyDown(KeyCode.Space))
+		{
+            rb.AddForce(Vector3.up * 10,ForceMode.Impulse);
+		}
+
     }
 }
